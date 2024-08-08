1baf2ae40c50ea0d8e448b709628c0019264b6ed
Gustavo Brunetto <gustavo.brunetto@earthdaily.com>
2024-08-08 14:27:13 -0700

    fix image repo

--- build-image.sh
+++ build-image.sh
@@ -4,5 +4,5 @@ set -e
 CODE_RELEASE=4.91.1
 
 # Build the Docker image
-docker build --build-arg CODE_RELEASE=$CODE_RELEASE -t harbor.eks.dec.earthdaily.com/pde/code-server:0.0.1 .
-docker push harbor.eks.dec.earthdaily.com/pde/code-server:0.0.1
+docker build --build-arg CODE_RELEASE=$CODE_RELEASE -t 590183725018.dkr.ecr.us-east-1.amazonaws.com/pde/code-server:0.0.1 .
+docker push 590183725018.dkr.ecr.us-east-1.amazonaws.com/pde/code-server:0.0.1
